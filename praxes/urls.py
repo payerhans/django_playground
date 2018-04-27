0704cdf44004302e6bc92656f0c4f07bb9acd5e4
Hans-Christian Payer <hans@amy.local.local>
2018-04-27 15:59:13 +0200

    url namespaces und manuelles menu

--- praxes/urls.py
+++ praxes/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.urls import path
 from . import views
 
+app_name = "praxes"
 urlpatterns = [
     path('', views.index, name='index'),
     path('jquery/', views.jquery, name='jqery'),
