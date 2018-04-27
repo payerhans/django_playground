527cefcdac4561917ca715666f03aad114234e90
=hans-Christian Payer <hans@net-so.org>
2018-04-27 13:49:58 +0200

    django menus hinzugefuegt

--- joe/settings.py
+++ joe/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',
+    'menu',
     'praxes'
 ]
 
@@ -64,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
             ],
         },
     },
